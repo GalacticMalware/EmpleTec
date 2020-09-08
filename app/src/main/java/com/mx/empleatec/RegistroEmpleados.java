c1b58a73dc1da1d5d282dbef0b043ea3fe885c67
Daniel Alejandro <daniel_mg_97@hotmail.com>
2020-09-08 07:15:40 -0600

    EmpleaTec

---
+++ app/src/main/java/com/mx/empleatec/RegistroEmpleados.java
@@ -0,0 +1,13 @@
+package com.mx.empleatec;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistroEmpleados extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_empleados);
+    }
+}
